c850dbc75319fb094196c48f523ced38ad81e180
Cecilia Raz <crrazak@gmail.com>
2018-06-01 10:25:56 -0400

    deployyy stuff I have no idea about

--- pages/index.vue
+++ pages/index.vue
@@ -46,14 +46,12 @@ Irvington, NY, 10533</strong></a></p>
 								<a href="/images/fulls/conserv.jpg" class="image fit"><img src="/images/thumbs/conservTH.jpg" title="The conservatory leads out to the side lawn." alt="" /></a></article>
 </div>
 <strong>Getting there</strong> 
+<p><em>Please do not Uber / Lyft:</em> drivers won't be able to get to the right entrance for drop off or pick up. Instead:<br></p>
 <p><em>Shuttle</em><br>
-	We'll be running a Shuttle from the Ardsley on Hudson metro station parking lot and nearby hotels starting around 1pm, and back around 9 or 10pm.</p>
+	We'll be running a Shuttle from the two recommended hotels starting around 1pm, and back in the evening. We'll also be picking folks up from the train station.</p>
 	<p><em>Driving / Taking the Train</em><br>Park at the Ardsley-on-Hudson Metro North station (there's limited parking on site), and take the shuttle (see above). It's also an easy 10 - 15 minute walk. The train runs every hour, a ~30min trip from Manhattan.</p>
 <img width=100% src="/images/fulls/VNmap3.jpg"> 
-<p><em>Uber / Lyft / Cab</em><br>
-	<em>Drop off:</em> Tell the driver to go all the way around the one-way roundabout and then make a left into 55 Hudson Road West.
-	<em>Pickup:</em> We can shuttle folks back to most nearby hotels. <em>(If you prefer to uber: use the address 55 Hudson Rd West — not your pin location.)</em>
-</p>
+
 
 
 
